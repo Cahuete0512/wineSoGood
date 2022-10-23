1b970140a048fda32102d2133319a2332ea72acf
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-10-23 15:30:29 +0200

    initial commit

---
+++ src/main/java/com/cahuete/winesogood/WineSoGoodApplication.java
@@ -0,0 +1,13 @@
+package com.cahuete.winesogood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineSoGoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineSoGoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cahuete/winesogood/WineSoGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahuete.winesogood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineSoGoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
